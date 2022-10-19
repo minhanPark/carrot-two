ceb476ec82b97daab5c3daf4cce87602066817cb
minhanPark <alsgks1218@naver.com>
2022-10-19 14:44:37 +0900

    #12.5

---
+++ pages/api/posts/[id]/answer.ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "../../../../libs/server/client";
+import withHandler, { Response } from "../../../../libs/server/withHandler";
+import { withApiSession } from "../../../../libs/server/withSession";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
+  const {
+    query: { id },
+    session: { user },
+    body: { answer },
+  } = req;
+
+  const newAnswer = await client.answer.create({
+    data: {
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+      post: {
+        connect: {
+          id: +id!.toString(),
+        },
+      },
+      answer,
+    },
+  });
+
+  res.json({ ok: true, answer: newAnswer });
+}
+
+export default withApiSession(withHandler({ methods: ["POST"], handler }));
